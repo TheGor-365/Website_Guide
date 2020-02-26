be355c9d5c3717cf6d395f358c7f23691f249b61
Gor <7495500@gmail.com>
2020-02-26 17:32:44 +0300

    WebSite_Style_Guide first commit

---
+++ public/functions.js
@@ -0,0 +1,113 @@
+// Colors
+
+function myFunction_1() {
+  var copyText = document.getElementById("2d5dcc");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function myFunction_2() {
+  var copyText = document.getElementById("d957d9");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function myFunction_3() {
+  var copyText = document.getElementById("4fe0b0");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function myFunction_4() {
+  var copyText = document.getElementById("efd9ca");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function myFunction_5() {
+  var copyText = document.getElementById("F0F8FF");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function myFunction_6() {
+  var copyText = document.getElementById("9932CC");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+//Fonts
+
+function copy_source_code_pro_1() {
+  var copyText = document.getElementById("source_code_pro_1");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_source_code_pro_2() {
+  var copyText = document.getElementById("source_code_pro_2");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_poppins_1() {
+  var copyText = document.getElementById("poppins_1");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_poppins_2() {
+  var copyText = document.getElementById("poppins_2");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_nutiro_sans_1() {
+  var copyText = document.getElementById("nutiro_sans_1");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_nutiro_sans_2() {
+  var copyText = document.getElementById("nutiro_sans_2");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_merriweather_sans_1() {
+  var copyText = document.getElementById("merriweather_sans_1");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_merriweather_sans_2() {
+  var copyText = document.getElementById("merriweather_sans_2");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+//Text css
+
+function copy_text_css_1() {
+  var copyText = document.getElementById("copy_css_h1");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_text_css_2() {
+  var copyText = document.getElementById("copy_css_h2");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_text_css_3() {
+  var copyText = document.getElementById("copy_css_p");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+function copy_text_css_4() {
+  var copyText = document.getElementById("copy_css_img");
+  copyText.select();
+  document.execCommand("copy");
+}
